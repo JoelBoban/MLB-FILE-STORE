675e141a95815d3dfd22caacfc4eeee52450bf9e
Joel Boban <90124641+JoelBoban@users.noreply.github.com>
2022-02-03 13:35:55 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -2,15 +2,17 @@
 
 from pyrogram import __version__
 from bot import Bot
-from config import OWNER_ID
+
+from config import OWNER_ID, OWNER_NAME
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>ᴇʟᴏɴᴍᴜsᴋ</a>\n○ Maintained by : <a href='t.me/joel_boban'>Jσєℓ Bσвαη [Oғғlιɴe]</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a></b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>{OWNER_NAME}</a>\n○ Maintained by : <a href='t.me/joel_boban'>Jσєℓ Bσвαη [Oғғlιɴe]</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
